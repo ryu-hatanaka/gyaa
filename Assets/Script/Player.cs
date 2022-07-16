dd4d45b1e9b252f77b0e39e3a3360766ee449ce0
tcagame <tcagame@tca.ac.jp>
2022-07-16 18:04:53 +0900

    1001 プレイヤーの移動

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -23,40 +23,36 @@ public class Player : Mother {
 
     private void move( ) {
         MoveToKey( );
-        MoveToNavigation( );
+        //MoveToNavigation( );
     }
 
     private void Update( ) {
 
     }
     /// <summary>マウスクリックで移動</summary>
-    private void MoveToNavigation( ) {
-        if( !Input.GetMouseButtonDown( 0 ) ) {
-            return;
-        }
+    //private void MoveToNavigation( ) {
+    //    if( !Input.GetMouseButtonDown( 0 ) ) {
+    //        return;
+    //    }
 
-        Ray ray = Camera.main.ScreenPointToRay( Input.mousePosition );
-        RaycastHit hit;
-        bool hashit = Physics.Raycast( ray, out hit );
-        if( hashit ) {
-            this.GetComponentInChildren<NavMeshAgent>( ).destination = hit.point;
-            Debug.Log( hit.point );
-        }
+    //    Ray ray = Camera.main.ScreenPointToRay( Input.mousePosition );
+    //    RaycastHit hit;
+    //    bool hashit = Physics.Raycast( ray, out hit );
+    //    if( hashit ) {
+    //        this.GetComponentInChildren<NavMeshAgent>( ).destination = hit.point;
+    //        Debug.Log( hit.point );
+    //    }
 
-    }
+    //}
 
     private void MoveToKey( ) {
-        if( Input.GetMouseButtonDown( 0 ) ) {
-            return;
-        }
+        
         
 
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
-        //キー入力時にナビゲーションの移動処理を消す
-        if(horizontal != 0 || vertical != 0){
-            this.GetComponentInChildren<NavMeshAgent>( ).ResetPath() ;
-        }
+       
+        
         float speed = COMMON.VAR.PLAYER_SPEED;
         Vector3 vec = new Vector3(horizontal * speed, 0.0f, vertical * speed);
         rb.velocity = vec;
