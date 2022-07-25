6681ee89fcfd29657228acc2e39e0607079e02a9
tca_hatanaka <moairyunosuke@gmail.com>
2022-07-25 13:17:35 +0900

    hatanaka

--- Assets/Script/Mother.cs
+++ Assets/Script/Mother.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Mother : MonoBehaviour{
     protected List<GameObject> _henchman_list = new List<GameObject>( );
-    public void addHenchman( ){
+    public void createHenchman( ){
         GameObject prefab = ( GameObject )Resources.Load( "Henchman" );
         GameObject go = Instantiate( prefab );
         _henchman_list.Add( go );
--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -29,7 +29,7 @@ public class Player : Mother {
     }
 
     private void initCreateHenchman( ) {
-        addHenchman( );
+        createHenchman( );
     }
 }
 
